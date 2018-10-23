498d41cedb9d10f08e827a74f0f1345ed1580068
Andrew Whitehead <andrew@1crm.com>
2018-10-23 13:19:59 -0700

    remove _gh_pages from repo

--- _gh_pages/jekyll-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-CMD=${@-serve --incremental}
-docker run --rm -it --name=bootstrap-theme-jekyll \
-  -v $(pwd):/srv/jekyll -v bst-jekyll-bundle:/usr/local/bundle \
-  -p 4000:4000 -p 9001:9001 jekyll/jekyll jekyll $CMD
